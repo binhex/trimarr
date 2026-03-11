bfc1b9c06b5c1e56b199c61d83823d13181a0749
binhex <megalith01@gmail.com>
2026-03-11 18:42:47 +0000

    initial code

---
+++ src/trimarr/main.py
@@ -0,0 +1,5 @@
+# TODO scan media one file at a time based off the mediaa path specified in the CLI
+# TODO identify language tracks in media file
+# TODO identify subtitle tracks in media file
+# TODO identify title metadata in media file
+# TODO perform action to remove unwanted tracks, metadata etc from media file
